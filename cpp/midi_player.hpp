c6d2b34e75cfe3798e44fd4ab4c8b99a38f51262
schef <zvanstefan@gmail.com>
2019-02-22 13:41:07 +0100

    added midi_player cpp class

---
+++ cpp/midi_player.hpp
@@ -0,0 +1,28 @@
+#ifndef __MIDI_PLAYER_HPP__
+#define __MIDI_PLAYER_HPP__
+
+#include "RtMidi.h"
+
+class MidiPlayer
+{
+  private:
+    void (*_callbackFunction)(double deltatime, std::vector<unsigned char> *message, void *userData) = nullptr;
+    RtMidiIn *_midiInTweaker = nullptr;
+    RtMidiOut *_midiOutTweaker = nullptr;
+    RtMidiOut *_midiOut = nullptr;
+    static MidiPlayer *_instance;
+    MidiPlayer();
+    void init();
+    void chooseMidiInPort(RtMidiIn *rtmidi, char *searchName, char *portName);
+    void chooseMidiOutPort(RtMidiOut *rtmidi, char *searchName, char *portName);
+    void callbackFunction(double deltatime, std::vector<unsigned char> *message, void *userData);
+    static void staticCallbackFunction(double deltatime, std::vector<unsigned char> *message, void *userData);
+
+  public:
+    static MidiPlayer *getInstance();
+    void registerCallbackFunction(void (*callbackFunction)(double deltatime, std::vector<unsigned char> *message, void *));
+    void sendMessageOutTweaker( const std::vector<unsigned char> *message );
+    void sendMessageOut( const std::vector<unsigned char> *message );
+};
+
+#endif
