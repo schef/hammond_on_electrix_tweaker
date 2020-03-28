d9c4faddb3ce14fc5c1295a274d906af112732cb
schef <zvanstefan@gmail.com>
2020-03-28 11:03:35 +0000

    added connection fix for zynthian

---
+++ script/zynthian_midi_connections.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+jack_disconnect "ZynMidiRouter:main_in" "a2j:Tweaker [20] (capture): Tweaker MIDI 1"
+jack_disconnect "ZynMidiRouter:main_in" "a2j:Tweaker [20] (capture): Tweaker MIDI 2"
+jack_disconnect "ZynMidiRouter:main_in" "a2j:Midi Through [14] (capture): Midi Through Port-0"
+jack_disconnect "a2j:Midi Through [14] (capture): Midi Through Port-0" "ZynMidiRouter:main_in"
+jack_disconnect "a2j:Tweaker [20] (capture): Tweaker MIDI 1" "ZynMidiRouter:main_in"
+jack_disconnect "a2j:Tweaker [20] (capture): Tweaker MIDI 2" "ZynMidiRouter:main_in"
+jack_connect "a2j:tweaker [131] (capture): output" "ZynMidiRouter:main_in"
