f5c71848eb064c8b7976e5ecdc9ff1c12f3e9b10
schef <zvanstefan@gmail.com>
2020-03-31 08:11:37 +0100

    added files for startup

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,6 +1,9 @@
 #include <chrono>
 #include <stdlib.h>
 #include <thread>
+#include <chrono>
+
+using namespace std;
 
 #include "midi_parser.hpp"
 #include "midi_player.hpp"
@@ -16,6 +19,9 @@ int main() {
 
   printf(FILE_NAME "Press any key to exit.\n");
 
+  auto start = chrono::steady_clock::now();
+  uint32_t keepAlive = 0;
+
   // while (true)
   // {
   // MidiPlayer::getInstance()->sendMessageOutTweaker(0x90, 0x30, 0x64);
@@ -24,6 +30,14 @@ int main() {
   // std::this_thread::sleep_for(std::chrono::milliseconds(500));
   // }
 
-  getchar();
+  //getchar();
+  
+  while(true){
+    if (chrono::duration_cast<chrono::seconds>(chrono::steady_clock::now() - start).count() >= 5) {
+      start = chrono::steady_clock::now();
+      printf("keepAlive: %d\n", keepAlive);
+      keepAlive++;
+    }
+  }
   return 0;
 }
--- script/tweakerPatch.sh
+++ script/tweakerPatch.sh
@@ -6,4 +6,8 @@ jack_disconnect "ZynMidiRouter:main_in" "a2j:Midi Through [14] (capture): Midi T
 jack_disconnect "a2j:Midi Through [14] (capture): Midi Through Port-0" "ZynMidiRouter:main_in"
 jack_disconnect "a2j:Tweaker [20] (capture): Tweaker MIDI 1" "ZynMidiRouter:main_in"
 jack_disconnect "a2j:Tweaker [20] (capture): Tweaker MIDI 2" "ZynMidiRouter:main_in"
-jack_connect "a2j:tweaker [131] (capture): output" "ZynMidiRouter:main_in"
+aconnect 129 20
+aconnect 20 128
+jack_connect "a2j:tweaker [130] (capture): output" "ZynMidiRouter:main_in"
+
+exit 0
