9c518d79106dcc2763ad1f4f00cf139d9241715d
schef <zvanstefan@gmail.com>
2020-04-02 09:29:12 +0200

    testing

--- cpp/midi_parser.cpp
+++ cpp/midi_parser.cpp
@@ -349,7 +349,7 @@ void MidiParser::parseNoteOn(uint8_t byte1, uint8_t byte2) {
     printf("BUTTON_TRACK_SELECT %d\n", byte2);
 #define ZYNTHIAN_UI_SELECT 51
     if (byte2 == 0) {
-      MidiPlayer::getInstance()->sendMessageOut(MidiParser::NOTE_ON_C16, ZYNTHIAN_UI_SELECT, 0);
+      MidiPlayer::getInstance()->sendMessageOut(MidiParser::NOTE_ON_C16, ZYNTHIAN_UI_SELECT, 100);
     }
     break;
 
@@ -452,9 +452,9 @@ void MidiParser::parseControlChange(uint8_t byte1, uint8_t byte2) {
 #define ZYNTHIAN_UI_SELECT_UP 52
 #define ZYNTHIAN_UI_SELECT_DOWN 53
     if (byte2 == TRACK_SELECT_CLOCKWISE) {
-      MidiPlayer::getInstance()->sendMessageOut(MidiParser::NOTE_ON_C16, ZYNTHIAN_UI_SELECT_DOWN, 0);
+      MidiPlayer::getInstance()->sendMessageOut(MidiParser::NOTE_ON_C16, ZYNTHIAN_UI_SELECT_DOWN, 100);
     } else if (byte2 == TRACK_SELECT_ANTICLOCKWISE) {
-      MidiPlayer::getInstance()->sendMessageOut(MidiParser::NOTE_ON_C16, ZYNTHIAN_UI_SELECT_UP, 0);
+      MidiPlayer::getInstance()->sendMessageOut(MidiParser::NOTE_ON_C16, ZYNTHIAN_UI_SELECT_UP, 100);
     }
     break;
 
